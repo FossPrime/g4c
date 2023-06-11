af48b25776df2d8791c7dfe55ca3d1ef6d8092b4
Ray Foss <git@vblip.com>
2023-06-11 02:54:15 +0000

    switched to pnpm

--- bin/demo.sh
+++ bin/demo.sh
@@ -1,16 +1,29 @@
-#!/bin/bash
-set -euox pipefail
 export npm_config_yes=true 
+DEMODIR=DEMO-`date +%FT%TZ`
+mkdir $DEMODIR
+cd $DEMODIR
+sleep 1
 
 g4c clone 'https://github.com/piuccio/cowsay.git'
+
+sleep 1
 g4c clone 'https://github.com/piuccio/cowsay.git' MOO
 
 cd cowsay
 touch grass.md
+sleep 1
 g4c status
+sleep 1
 
 g4c add --all
+sleep 1
+
 g4c status
+sleep 1
+
+cd "../../$DEMODIR"
+echo "cloning LowDB in place"
+g4c clone 'https://github.com/typicode/lowdb' './'
 
 g4c commit -m 'touched grass...'
 # git push
